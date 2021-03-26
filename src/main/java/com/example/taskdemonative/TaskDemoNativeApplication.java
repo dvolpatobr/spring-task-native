7107b7157a66c12b2ffd1f1a062341a246e5702b
VOLPATO, DANILO <danilo.volpato@servexternos.gruposantander.es>
2021-03-26 14:00:16 +0100

    force version of jdbc

--- src/main/java/com/example/taskdemonative/TaskDemoNativeApplication.java
+++ src/main/java/com/example/taskdemonative/TaskDemoNativeApplication.java
@@ -1,5 +1,6 @@
 package com.example.taskdemonative;
 
+import org.mariadb.jdbc.util.DefaultOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.batch.configuration.JobLaunchCondition;
@@ -21,21 +22,27 @@ import static org.springframework.nativex.hint.AccessBits.ALL;
 				"org.springframework.cloud.task.batch.configuration.JobLaunchCondition",
 				"org.springframework.cloud.task.batch.configuration.JobLaunchCondition$SpringBatchJobCondition",
 				"org.springframework.cloud.task.batch.configuration.JobLaunchCondition$FailOnJobFailureCondition",
+				"org.mariadb.jdbc.util.DefaultOptions"
 		}, packageNames = {
-				"org.springframework.cloud.task.batch.configuration"
+				"org.springframework.cloud.task.batch.configuration",
+				"org.mariadb.jdbc.util"
 		}, initTime = InitializationTime.BUILD),
 		types = {
-				@TypeHint(types = JobLaunchCondition.class, access = PUBLIC_METHODS | DECLARED_FIELDS | DECLARED_METHODS | DECLARED_CONSTRUCTORS | ALL),
+				@TypeHint(types = {JobLaunchCondition.class, DefaultOptions.class}, access = PUBLIC_METHODS | DECLARED_FIELDS | DECLARED_METHODS | DECLARED_CONSTRUCTORS | ALL),
 		}
 
 
 )
-@TypeHint(types = JobLaunchCondition.class, typeNames = {"org.springframework.cloud.task.batch.configuration.JobLaunchCondition$SpringBatchJobCondition","org.springframework.cloud.task.batch.configuration.JobLaunchCondition$FailOnJobFailureCondition"}, access = ALL)
+@TypeHint(types = {JobLaunchCondition.class, DefaultOptions.class}, typeNames = {"org.springframework.cloud.task.batch.configuration.JobLaunchCondition$SpringBatchJobCondition","org.springframework.cloud.task.batch.configuration.JobLaunchCondition$FailOnJobFailureCondition","org.mariadb.jdbc.util.DefaultOptions"}, access = ALL)
 @EnableTask
 public class TaskDemoNativeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskDemoNativeApplication.class, args);
+
+		DefaultOptions
+
+
 	}
 
 }
